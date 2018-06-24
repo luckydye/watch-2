0ee299d6a8fefe8e7aff6a3ebcb8a62a76fdec76
Tim Hav <tim.h4vlicek@gmail.com>
2018-06-24 12:04:14 +0200

    layout

--- src/client/js/helpers.js
+++ src/client/js/helpers.js
@@ -1,12 +1,29 @@
 
-function createNotification(text, time) {
+function displayNotification(text, time, onclick) {
 	const notes = document.querySelector("w2-notifications");
-	const note = document.createElement("w2-notification");
-	note.innerHTML = text;
-	note.timer = time;
-	setTimeout(() => {
-		note.style.setProperty("animation", "fade-out .3s ease");
-		setTimeout(() => { note.remove(); }, 300);
-	}, note.timer);
-	notes.appendChild(note);
+	if(notes) {
+
+		const note = document.createElement("w2-notification");
+		note.innerHTML = text;
+		note.timer = time;
+
+		const timer = setTimeout(() => {
+			close();
+		}, note.timer);
+
+		function close() {
+			note.style.setProperty("animation", "fade-out .25s ease");
+			setTimeout(() => { note.remove(); }, 200);
+		}
+
+		note.onclick = () => {
+			clearTimeout(timer);
+			if(onclick) {
+				onclick(note);
+			}
+			close();
+		}
+
+		notes.appendChild(note);
+	}
 }
